e2160f5de87c72435b2f661f7822b35586775181
Antoinette Smith <antoinette@primeacademy.io>
2016-05-18 14:28:24 -0500

    Final class example with PUT and DELETE

--- server/public/scripts/clientapp.js
+++ server/public/scripts/clientapp.js
@@ -3,7 +3,64 @@ $(document).ready(function () {
 
   // add a movie
   $('#movieSubmit').on('click', postMovie);
+
+  // event listeners for Movies list
+  $('#movieList').on('click', '.update', putMovie);
+  $('#movieList').on('click', '.delete', deleteMovie);
 });
+
+/**-------- UTILITY FUNCTIONS --------**/
+function dataPrep(button) {
+  // get the movie data
+  var movie = {};
+  console.log(button.parent().children());
+  console.log(button.parent().children().serializeArray());
+  $.each(button.parent().children().serializeArray(), function (i, field) {
+    movie[field.name] = field.value;
+  });
+
+  console.log('dataPrep', movie);
+
+  return movie;
+}
+
+function getMovieId(button) {
+  // get the movie ID
+  var movieId = button.parent().data('movieId');
+  console.log('getMovieId', movieId);
+  return movieId;
+}
+
+/**-------- AJAX FUNCTIONS --------**/
+function putMovie(event) {
+  event.preventDefault();
+
+  var preparedData = dataPrep($(this));
+  var movieId = getMovieId($(this));
+
+  $.ajax({
+    type: 'PUT',
+    url: '/movies/' + movieId,
+    data: preparedData,
+    success: function (data) {
+      getMovies();
+    },
+  });
+}
+
+function deleteMovie(event) {
+  event.preventDefault();
+
+  var movieId = getMovieId($(this));
+
+  $.ajax({
+    type: 'DELETE',
+    url: '/movies/' + movieId,
+    success: function (data) {
+      getMovies();
+    },
+  });
+}
 
 function getMovies() {
   $.ajax({
@@ -11,20 +68,28 @@ function getMovies() {
     url: '/movies',
     success: function (movies) {
       console.log(movies);
+      $('#movieList').empty();
       movies.forEach(function (movie) {
-        $('#movieList').append('<div>' + movie.title +
-          '|' + movie.year +
-          '|' + movie.genre +
-          '|' + movie.director +
-        '</div>');
+        $container = $('<div></div>');
+
+        // fields I want to edit
+        var movieProperties = ['title', 'year', 'director'];
+        movieProperties.forEach(function (prop) {
+          var $el = $('<input type="text" id="' + prop + '" name="' + prop + '" />');
+          $el.val(movie[prop]);
+          $container.append($el);
+        });
+
+        $container.data('movieId', movie.movie_id);
+        $container.append('<button class="update">Update</button>');
+        $container.append('<button class="delete">Delete</button>');
+        $('#movieList').append($container);
       });
     },
   });
-
-
 }
 
-function postMovie() {
+function postMovie(event) {
   event.preventDefault();
 
   var movie = {};
@@ -38,7 +103,7 @@ function postMovie() {
     url: '/movies',
     data: movie,
     success: function (data) {
-      console.log('Successful post!');
+      getMovies();
     },
   });
 }
--- server/routes/movies.js
+++ server/routes/movies.js
@@ -21,7 +21,7 @@ router.get('/', function (req, res) {
 
 router.post('/', function (req, res) {
   var movie = req.body;
-  console.log(movie);
+
   pg.connect(connectionString, function (err, client, done) {
     if (err) {
       res.sendStatus(500);
@@ -42,5 +42,61 @@ router.post('/', function (req, res) {
                  });
   });
 });
+
+router.put('/:id', function (req, res) {
+  var id = req.params.id;
+  var movie = req.body;
+  console.log(req.body);
+
+  pg.connect(connectionString, function (err, client, done) {
+    if (err) {
+      res.sendStatus(500);
+    }
+
+    client.query('UPDATE movies ' +
+                  'SET title = $1, ' +
+                  'year = $2, ' +
+                  'director = $3' +
+                  'WHERE movie_id = $4',
+                   [movie.title, movie.year, movie.director, id],
+                 function (err, result) {
+                   done();
+
+                   if (err) {
+                     res.sendStatus(500);
+                     return;
+                   }
+
+                   res.sendStatus(200);
+                 });
+  });
+});
+
+router.delete('/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(id);
+  pg.connect(connectionString, function (err, client, done) {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    }
+
+    client.query('DELETE FROM movies ' +
+                  'WHERE movie_id = $1',
+                   [id],
+                 function (err, result) {
+                   done();
+
+                   if (err) {
+                     console.log(err);
+                     res.sendStatus(500);
+                     return;
+                   }
+
+                   res.sendStatus(200);
+                 });
+  });
+});
+
 
 module.exports = router;
